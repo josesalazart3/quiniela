dfa13d2b8d2eec3ec1c80f6fcdd7c1c6d36dd245
Jose Leonel Salazar Tejeda <139662279+josesalazart3@users.noreply.github.com>
2026-05-29 14:47:07 -0600

    Delete IEmailService interface
    Removed IEmailService interface and its methods.

--- Services/Interfaces/IEmailService.cs
+++ Services/Interfaces/IEmailService.cs
@@ -1,9 +1,1 @@
-namespace Quiniela.Services.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendInvitacionLigaAsync(string emailDestino, string nombreLiga, string token);
-        Task SendAprobacionMiembroAsync(string emailDestino, string nombreLiga);
-        Task SendRecuperacionPasswordAsync(string emailDestino, string token);
-    }
-}
+
